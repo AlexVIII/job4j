d176c40c8ac6aa44444553532e71d6b79007c10d
AlexVIII <stockon@yandex.ru>
2018-04-13 15:17:34 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,15 +1,6 @@
-
-
-*/**
-* Package for calculate task.
-*
-* @author Aleksandr Vasyanin (stockon@yandex.ru)
-* @version $Id$
-* @since 12.04/2018
-*/
 package ru.job4j;
 
-public class Calculate{
+public class Calculate {
 public static void main (String [] args){
 	System.out.println("Hello world");
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+*/**
+* Package for calculate task.
+*
+* @author Aleksandr Vasyanin (stockon@yandex.ru)
+* @version $Id$
+* @since 12.04/2018
+*/
