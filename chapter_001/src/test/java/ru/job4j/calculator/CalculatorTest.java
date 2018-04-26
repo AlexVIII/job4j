ffc592c55a5da227319960aa1d5a4dad37b6e386
AlexVIII <stockon@yandex.ru>
2018-04-26 05:57:55 +0300

    3.1. Элементарный калькулятор. [#185]

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -9,9 +9,34 @@ public class CalculatorTest {
     @Test
     public void whenAddOnePlusOneThenTwo() {
         Calculator calc = new Calculator();
-        calc.add(1D, 1D);
+        calc.add(1, 1);
         double result = calc.getResult();
-        double expected = 2D;
+        double expected = 2;
         assertThat(result, is(expected));
     }
+    @Test
+    public void whenSubtractOneMinusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.subtract(1, 1);
+        double result = calc.getResult();
+        double expected = 0;
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenSubtractOneDelimOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.div(2, 1);
+        double result = calc.getResult();
+        double expected = 2;
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenMultipleOneUmnozhOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.multiple(4, 2);
+        double result = calc.getResult();
+        double expected = 8;
+        assertThat(result, is(expected));
+    }
+
 }
