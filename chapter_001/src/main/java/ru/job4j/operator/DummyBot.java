c61978cc666d3bc0dda9509ce299284387668fd2
AlexVIII <stockon@yandex.ru>
2018-05-15 07:19:06 +0300

    3.1. Глупый бот. [#31323]

---
+++ chapter_001/src/main/java/ru/job4j/operator/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.operator;
+/*
+    * @author Aleksandr Vasyanin
+    * @version 1.0
+    * @since 14/05/2018
+     */
+public class DummyBot {
+    /**
+     * Отвечаем на вопросы.
+     * @param question Вопрос клиента.
+     * @return Ответ.
+     */
+    public String answer(String question){
+        String rsl = "Это ставит меня в тупик.Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)){
+            rsl = "Привет, умник.";}
+            else if ("Пока".equals(question)){
+            rsl = "До скорой встречи.";
+        }
+            return rsl;
+
+    }
+
+}
