188d2394736ff3a689b0d15967e0753feb2c4367
AlexVIII <stockon@yandex.ru>
2018-05-17 06:53:12 +0300

    3.4. Расстояние между точками в системе координат [#188]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,12 +1,11 @@
 package ru.job4j.condition;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 /**
  * @author Aleksandr Vasyain (stockon@yandex.ru)
  * @version 1.0
  * @since 12/05/2018
-  */
+ */
 
 
 class Point {
@@ -20,25 +19,24 @@ class Point {
     public double distanceto(Point that) {
         Point a = this;
         Point b = that;
-     /** int x1 = a.x;
+        int x1 = a.x;
         int x2 = b.x;
         int y1 = a.y;
         int y2 = b.y;
 
-       System.out.println("x1 = "+ a.x);
-        System.out.println("y1 = "+ a.y);
-        System.out.println("x2 = "+ b.x);
-        System.out.println("y2 = + "+ b.y);
-    **/
-     double result = 2 * (Math.pow(this.x - this.x, that.y) + Math.pow(this.y - that.y, that.x)) / ((this.x + that.x) * (this.y + that.y));
-          return result;
+        /** System.out.println("x1 = "+ a.x);
+         System.out.println("y1 = "+ a.y);
+         System.out.println("x2 = "+ b.x);
+         System.out.println("y2 = + "+ b.y);
+         **/
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
-public static void main(String[] args) {
+    public static void main(String[] args) {
         Point a = new Point(3, 2);
         Point b = new Point(2, 5);
-    double result = a.distanceto(b);
-    System.out.println("Расстояние между двумя точками А и В = " + result);
-}
+        double result = a.distanceto(b);
+        System.out.println("Расстояние между двумя точками А и В = " + result);
+    }
 
 
 }
