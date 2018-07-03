03fdf3e1aa65f703919930568c0f5ac87a3a98b5
AlexVIII <stockon@yandex.ru>
2018-07-03 17:03:35 +0300

    Валидация

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -4,9 +4,9 @@ public class Square {
     public int[] calculate(int bound) {
         int[] rst = new int[bound];
         // заполнить массив через цикл элементами от 1 до bound возведенными в квадрат
-        for (int index: rst) {
-
-            return rst;
-        }
+        for (int index = 0; index <= rst.length; index++) {
+           rst[index] = rst[index] * rst[index];
+           }
+        return rst;
     }
 }
