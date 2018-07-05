ec69e48e4b9d582547892eafee66e3d1958a12c5
AlexVIII <stockon@yandex.ru>
2018-07-05 15:09:27 +0300

    6.0. Заполнить массив степенями чисел. [#33488]

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -1,12 +1,13 @@
 package ru.job4j.array;
 
 public class Square {
-    public int[] calculate(int bound) {
+    public int[] calculate(int bound, int ix, int iy) {
         int[] rst = new int[bound];
         // заполнить массив через цикл элементами от 1 до bound возведенными в квадрат
-       int ix = 1;
+
         for (int index = 0; index < bound; index++) {
-                   rst[index] = (ix+index) * (ix+index);
+                   rst[index] = ix * ix;
+                   ix = ix +iy;
         }
         return rst;
 
--- chapter_001/src/test/java/ru/job4j/array/SquareTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -7,10 +7,29 @@ package ru.job4j.array;
 public class SquareTest {
     @Test
     public void whenBound3Then149() {
+        int ix = 1, iy = 1;
         int bound = 3;
         Square square = new Square();
-        int[] rst = square.calculate(bound);
+        int[] rst = square.calculate(bound, ix, iy);
         int[] expect = new int[] {1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+    @Test
+    public void whenBound3Then567() {
+        int ix = 2, iy = 1;
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.calculate(bound, ix, iy);
+        int[] expect = new int[] {4, 9, 16};
+        assertThat(rst, is(expect));
+    }
+    @Test
+    public void whenBound3Then357() {
+        int ix = 2, iy = 2;
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.calculate(bound, ix, iy);
+        int[] expect = new int[] {4, 16, 36};
         assertThat(rst, is(expect));
     }
 }
