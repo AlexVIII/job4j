5c0bbdac8459ecb55d4e181d9fd1b0f6ff585b2c
AlexVIII <stockon@yandex.ru>
2018-07-12 16:43:29 +0300

    6.5. Создать программу для сортировки массива методом перестановки. [#195]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,10 +3,10 @@ package ru.job4j.array;
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data.length; j++) {
+        for (int i = 1; i < data.length; i++) {
+            for (int j = 1; j < data.length; j++) {
                 if ( i == j  ){
-                    if (data[i][j] != true) {
+                    if (data[i][j] != data[i-1][j-1]) {
                         result = false;
                         break;
                     }
