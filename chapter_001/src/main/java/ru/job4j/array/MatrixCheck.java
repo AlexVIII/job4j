5210dff9d8cf01dcb2b37323a527b4d337084759
AlexVIII <stockon@yandex.ru>
2018-07-17 10:51:53 +0300

    Валидация

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -9,8 +9,25 @@ public class MatrixCheck {
                         result = false;
                         break;
                     }
+
                }
 
+        for (int i = 0; i < data.length-2; i++) {
+
+            if (data[i+1][i] != data[i+2][i+1]) {
+                result = false;
+                break;
+            }
+
+        }
+        for (int i = 0; i < data.length-2; i++) {
+
+            if (data[i][i+1] != data[i+1][i+2]) {
+                result = false;
+                break;
+            }
+
+        }
 
         return result;
     }
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -22,8 +22,8 @@ public class MatrixCheckTest {
         MatrixCheck check = new MatrixCheck();
         boolean[][] input = new boolean[][] {
                 {true, true, false},
-                {false, false, true},
-                {true, false, true}
+                {false, true, false},
+                {true, true, true}
         };
         boolean result = check.mono(input);
         assertThat(result, is(false));
