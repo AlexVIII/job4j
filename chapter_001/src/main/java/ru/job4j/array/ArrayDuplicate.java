702a8d618d2e4d68bbbbab78260519faf5452f59
AlexVIII <stockon@yandex.ru>
2018-07-20 14:15:31 +0300

    6.8. Удаление дубликатов в массиве.

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -1,13 +1,21 @@
 package ru.job4j.array;
 
-public class ArrayDuplicate {
-    public String[] exit(String[] data){
-        int end = data.length;
-        for (int i = 1, i < end, i++) {
-            if ( data[i] == data[end]){
-                end = end -1;
-            }
-        }
+import java.util.Arrays;
 
-    }
+public class ArrayDuplicate {
+    public String[] exit(String[] array){
+        int abc = array.length;
+        for (int out = 0; out < abc; out++) {
+            for (int in = out + 1; in < abc; in++){
+                if (array[out].equals(array[in])) {
+                    array[in] = array [abc - 1];
+                    abc--;
+                    in--;
+                }
+            }
+
+        }
+   return Arrays.copyOf(array, abc);
+
+       }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -2,16 +2,16 @@ package ru.job4j.array;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
+import static org.junit.Assert.*;
 
 public class ArrayDuplicateTest {
     @Test
-    public void DeleteDublicateInArray() {
+    public void DeleteDublicatedInArray() {
         ArrayDuplicate duplicate = new ArrayDuplicate();
-        String[] input = new String[]{рожь, пшеница, семена, лен, рис, лен};
+        String[] input = {"рожь", "пшеница", "семена", "лен", "рис", "лен"};
         String[] result = duplicate.exit(input);
-        String[] expert = new String[]{рожь, пшеница, семена, лен, рис};
-        assertThat(result,is(expert());
+        String[] except = {"рожь", "пшеница", "семена", "рис", "лен"};
+        assertThat(result,arrayContainingInAnyOrder(except));
     }
 }
