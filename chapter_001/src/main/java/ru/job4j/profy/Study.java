3b588b2d5dc46544fa8d4bb6fb4756f10c4e9be1
AlexVIII <stockon@yandex.ru>
2018-09-28 15:09:33 +0300

    2. Чтение кода и базовые ошибки. [#40205]

---
+++ chapter_001/src/main/java/ru/job4j/profy/Study.java
@@ -0,0 +1,4 @@
+package ru.job4j.profy;
+
+public class Study extends Teacher {
+}
---
+++ chapter_001/src/main/java/ru/job4j/profy/Teacher.java
@@ -0,0 +1,7 @@
+package ru.job4j.profy;
+
+public class Teacher extends Profession {
+}
+public Study (Student student){
+
+}
