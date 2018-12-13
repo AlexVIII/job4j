d9f7552f9a35e25b4a6fb0438ed6df6d9bfebb0b
AlexVIII <stockon@yandex.ru>
2018-12-13 16:19:52 +0300

    ConsoleInput в классе StartUI для Tracker

---
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,116 @@
+package ru.job4j.tracker;
+
+import ru.job4j.start.*;
+
+/**
+ * @version $Id$
+ * @since 0.1
+ */
+public class StartUI {
+    /**
+     * Константа меню для добавления новой заявки.
+     */
+    private static final String ADD = "0";
+    private static final String SHOW = "1";
+    private static final String EDIT = "2";
+    private static final String DELL = "3";
+    private static final String FINDid = "4";
+    private static final String FINDname = "5";
+
+    /**
+     * Константа для выхода из цикла.
+     */
+    private static final String EXIT = "6";
+    /**
+     * Получение данных от пользователя.
+     */
+    private final Input input;
+
+    /**
+     * Хранилище заявок.
+     */
+    private final Tracker tracker;
+
+    /**
+     * Конструтор инициализирующий поля.
+     * @param input ввод данных.
+     * @param tracker хранилище заявок.
+     */
+    public StartUI(Input input, Tracker tracker) {
+        this.input = input;
+        this.tracker = tracker;
+    }
+
+    /**
+     * Основой цикл программы.
+     */
+    public void init() {
+        boolean exit = false;
+        while (!exit) {
+            this.showMenu();
+            String answer = this.input.ask("Введите пункт меню : ");
+            if (ADD.equals(answer)) {
+                //добавление заявки вынесено в отдельный метод.
+                this.createItem();
+            }
+            else if (SHOW.equals(answer)) {
+                //показать все заявки
+                tracker.findAll();
+            }
+            else if (EDIT.equals(answer)) {
+            //отредактировать заявку
+            tracker.replace();
+            }
+            else if (DELL.equals(answer)) {
+                //удалить заявку
+                tracker.delete();
+            }
+            else if (FINDid.equals(answer)) {
+                //найти заявку по id
+                tracker.findById();
+            }
+            else if (FINDname.equals(answer)) {
+                //найти заявку по имени
+                tracker.findByName();
+            }
+            else if (EXIT.equals(answer)) {
+                exit = true;
+            }
+        }
+    }
+
+    /**
+     * Метод реализует добавленяи новый заявки в хранилище.
+     */
+    private void createItem() {
+        System.out.println("------------ Добавление новой заявки --------------");
+        String name = this.input.ask("Введите имя заявки :");
+        String desc = this.input.ask("Введите описание заявки :");
+        Item item = new Item(name, desc);
+        this.tracker.add(item);
+        System.out.println("------------ Новая заявка с getId : " + item.getId() + "-----------");
+    }
+
+    private void showMenu() {
+        System.out.println("Меню.");
+
+        // добавить остальные пункты меню.
+        System.out.println("0. Add new Item");
+        System.out.println("1. Show all items");
+        System.out.println("2. Edit item");
+        System.out.println("3. Delete item");
+        System.out.println("4. Find item by Id");
+        System.out.println("5. Find items by name");
+        System.out.println("6. Exit Program");
+    }
+
+    /**
+     * Запускт программы.
+     * @param args
+     */
+    public static void main(String[] args) {
+        new StartUI(new ConsoleInput(), new Tracker()).init();
+
+        String name =  input.ask("please, enter task's name");
+    }
+}
