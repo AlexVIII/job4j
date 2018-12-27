43dbfb9f6b4220abff6730b718059aa8ea5b1a34
AlexVIII <stockon@yandex.ru>
2018-12-27 16:23:48 +0300

    ConsoleInput в классе StartUI для Tracker

--- chapter_002/src/main/java/ru/job4j/start/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/start/ConsoleInput.java
@@ -5,7 +5,8 @@ import java.util.*;
 
 public  class ConsoleInput implements Input {
     private Scanner scanner = new Scanner(System.in);
-    public String ask(String guestion){
+
+    public String ask(String question){
         System.out.print(question);
         return  scanner.nextLine();
 
