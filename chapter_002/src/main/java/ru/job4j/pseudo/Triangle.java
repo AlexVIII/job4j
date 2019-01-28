7bda41a62ba5c2edd42c3d8101bc2bbf729bf61b
AlexVIII <stockon@yandex.ru>
2019-01-28 09:59:39 +0300

    5. Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
+++ chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
@@ -9,9 +9,9 @@ public class Triangle implements Shape{
     @Override
     public String draw() {
         StringBuilder pic = new StringBuilder();
-        pic.append("  +  ");
-        pic.append(" +  + ");
-        pic.append("++++++");
+                pic.append("  +  ");
+                pic.append(" + + ");
+                pic.append("+++++");
         return pic.toString();
     }
 }
--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -66,7 +66,6 @@ public class PaintTest {
                                         .add("  +  ")
                                         .add(" + + ")
                                         .add("+++++")
-                                        .add(System.lineSeparator())
                                         .toString()
                         )
                 );
