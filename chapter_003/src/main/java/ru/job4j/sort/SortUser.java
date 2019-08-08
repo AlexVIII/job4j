dc814ebbf53370e0bd5776cd9343d15be440242f
AlexVIII <stockon@yandex.ru>
2019-08-08 14:51:29 +0300

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,5 +1,6 @@
 package ru.job4j.sort;
 
+import java.util.Comparator;
 import java.util.TreeSet;
 import java.util.List;
 import java.util.Set;
@@ -9,4 +10,29 @@ class SortUser {
     public Set<User> sort(List<User> list) {
         return new TreeSet<User>(list);
     }
+
+    public void  SortNameLength(List<User> users){
+    users.sort(new Comparator<User>(){
+    @Override
+    public int compare(User o1, User o2){
+        return Integer.compare(o1.getName().length(), o2.getName().length());
+    }
+
+    }
+    );
+    }
+
+    public void sortByAllFields (List<User> users){
+        users.sort(new Comparator<User>(){
+            @Override
+            public int compare(User o1, User o2){
+                int result = o1.getName().compareTo(o2.getName());
+                return result != 0 ? result: Integer.compare(o1.getAge(), o2.getAge());
+            }
+
+        }
+        );
+
+    }
+
 }
--- chapter_003/src/main/java/ru/job4j/sort/User.java
+++ chapter_003/src/main/java/ru/job4j/sort/User.java
@@ -2,35 +2,38 @@ package ru.job4j.sort;
 
 class User implements Comparable<User> {
 
-      private String name;
-      private String city;
-      public int age;
+    private String name;
+    private String city;
+    public int age;
 
-      public User (String name, int age, String city){
-          this.name = name;
-          this.city = city;
-          this.age = age;
-        }
-        public int getAge(){
-            return this.age;
-        }
+    public User(String name, int age) {
+        this.name = name;
+        //this.city = city;
+        this.age = age;
+    }
 
-        public String getName() {
-            return this.name;
-        }
+    public int getAge() {
+        return this.age;
+    }
 
+
+    public String getName() {
+        return this.name;
+    }
+
+
+        /*
         public String getCity() {
-            return this.city;
+              return this.city;
         }
-
+         */
 
 
     @Override
     public int compareTo(User user) {
-        int result = Integer.compare(this.age, user.age);
-        return result != 0 ? Integer.compare(this.age, getAge()) : this.name.compareTo(user.name);
+        return Integer.compare(this.age, getAge());
     }
-    }
+}
 
 
 
