c23c00eee5675bf839df84aafa05311c4c600eab
AlexVIII <stockon@yandex.ru>
2019-08-08 15:12:59 +0300

    Шилдт.рекурсия

---
+++ chapter_003/src/main/java/ru/job4j/Shildt/Rekursia/Factorial.java
@@ -0,0 +1,18 @@
+package ru.job4j.Shildt.Rekursia;
+
+public class Factorial {
+    int fact(int n){
+        int result;
+        if (n == 1) return 1;
+        result =  fact(n-1)* n;
+        return result;
+    }
+    }
+    class Recursion{
+        public static void main(String[] args) {
+            Factorial f = new Factorial();
+            System.out.println("Факториал 3 = " + f.fact(3));
+            System.out.println("Факториал 5 = " + f.fact(5));
+            System.out.println("Факториал 10 = " + f.fact(10));
+        }
+    }
