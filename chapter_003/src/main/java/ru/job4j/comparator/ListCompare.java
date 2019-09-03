73442a9600ffb2dc5a382aefe858caef3d098f80
AlexVIII <stockon@yandex.ru>
2019-09-03 10:41:02 +0300

    3. Сортировка[#10015][#134050]

---
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -0,0 +1,30 @@
+package ru.job4j.comparator;
+
+import java.util.Comparator;
+
+
+public class ListCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int rst1 = 0;
+        int l = left.length();
+        int r = right.length();
+        for (int i = 0; i < Math.min(l,r);i++ ) {
+            char cl = left.charAt(i);
+            char cr = right.charAt(i);
+            rst1 = Character.compare(cl, cr);
+            if (rst1 != 0) {
+                break;
+        }
+
+            return rst1;
+
+        }
+
+
+
+    }
+
+
+
+}
