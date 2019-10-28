cb12fe0dce016368f55ba14500c708fe74f6733b
AlexVIII <stockon@yandex.ru>
2019-10-28 14:59:22 +0300

    Банковские переводы. [#10038]

---
+++ chapter_003/src/main/java/ru/job4j/banki/account.java
@@ -0,0 +1,14 @@
+package ru.job4j.banki;
+
+public class Account {
+    private double value;
+     private int requisites;
+
+    public Account(double value, int requisites){
+        this.value = value;
+        this.requisites = requisites;
+
+    }
+
+
+}
