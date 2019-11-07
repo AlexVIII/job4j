d016db509824e3f97c1bbb3028d7a741f327cdd6
AlexVIII <stockon@yandex.ru>
2019-11-07 15:20:04 +0300

    Банковские переводы. [#10038]

---
+++ chapter_003/src/main/java/ru/job4j/client/addUser.java
@@ -0,0 +1,15 @@
+package ru.job4j.client;
+
+import ru.job4j.list.User;
+
+public class addUser {
+    private String name;
+    private Integer passport;
+
+    private addUser(User user){
+        this.name = name;
+        this.passport = passport;
+    }
+
+
+}
---
+++ chapter_003/src/main/java/ru/job4j/client/bank.java
@@ -0,0 +1,37 @@
+package ru.job4j.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class bank {
+    public static void main(String[] args) {
+       String name, account;
+       Integer passport;
+
+
+
+
+        /*добавление пользователя*/
+        public void addUser (User user){
+            Map<String, Integer> client = new HashMap<String, Integer>();
+            client.put(addUser.name, addUser.passport);
+        }
+/*
+        public void deleteUser(User user) {
+            client.remove(name);
+
+
+        }
+
+    public void addAccountToUser(String passport, Account account) {
+
+    } - добавить счёт пользователю.
+        }
+
+    public void deleteAccountFromUser(String passport, Account account) {} - удалить один счёт пользователя.
+
+    public List<Accounts> getUserAccounts (String passport) {} - получить список счетов для пользователя.
+    }
+*/
+
+}
